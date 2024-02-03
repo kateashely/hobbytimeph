98b333c2a63f060e68e2a825bf382df2add730be
kateashely <153527247+kateashely@users.noreply.github.com>
2024-02-03 22:44:48 +0800

    Update TitleScreen.js

--- hobbytimeph/src/TitleScreen.js
+++ hobbytimeph/src/TitleScreen.js
@@ -1,25 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const TitleScreen = () => {
+function TitleScreen() {
   return (
-    <div className="body">
-      <div className="title">
-        <h1>Maze of the One Piece</h1>
-        <div className="title-btn">
-        <a href="/Level1">
-            <button>Start Game</button>
-          </a>
-          <Link to="/Instructions">
-            <button>Instructions</button>
-          </Link>
-          <Link to="/About">
-            <button>About Us</button>
-          </Link>
+    <div className="App">
+      <nav>
+        <div className="menu">
+          <div className="logo">
+            <Link to="/">Hobby Time</Link> 
+          </div>
+          <ul>
+            <li><Link to="/">Home</Link></li> 
+            <li><Link to="/brands">Brands</Link></li> 
+            <li><Link to="About">About Us</Link></li> 
+          </ul>
+        </div>
+      </nav>
+      <div className="img"></div>
+      <div className="center">
+        <div className="title">WELCOME</div>
+        <div className="sub_title">CO-HOBBYIST!</div>
+        <div className="btns">
+          <button><Link to="/products">All Products</Link></button>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default TitleScreen;
